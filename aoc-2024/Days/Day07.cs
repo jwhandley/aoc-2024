f2d526dca6601d496fc4038e965a53f0235a50d5
John Handley <jwhandley17@gmail.com>
2024-12-07 11:20:02 +0000

    Clean up day 7

--- aoc-2024/Days/Day07.cs
+++ aoc-2024/Days/Day07.cs
@@ -1,6 +1,6 @@
 namespace aoc_2024.Days;
 
-public class Day07 :BaseDay
+public class Day07 : BaseDay
 {
     private readonly Equation[] equations;
 
@@ -15,21 +15,40 @@ public class Day07 :BaseDay
         }).ToArray();
     }
 
-    public override long Part1() => equations.AsParallel().Where(e => SolveRecursive(e.Target, e.Numbers, e.Numbers.Length-1)).Sum(e => e.Target);
-    
-    public override long Part2() => equations.AsParallel().Where(e => SolveRecursive(e.Target, e.Numbers, e.Numbers.Length-1, true)).Sum(e => e.Target);
+    public override long Part1() => equations.AsParallel()
+        .Where(e => SolveRecursive(e.Target, e.Numbers, e.Numbers.Length - 1)).Sum(e => e.Target);
+
+    public override long Part2() => equations.AsParallel()
+        .Where(e => SolveRecursive(e.Target, e.Numbers, e.Numbers.Length - 1, true)).Sum(e => e.Target);
 
     private static bool SolveRecursive(long target, long[] numbers, int idx, bool part2 = false)
     {
-        if (idx == 0) return target == numbers[idx];
-        if (target % numbers[idx] == 0 && SolveRecursive(target / numbers[idx], numbers, idx - 1, part2)) return true;
-        if (target > numbers[idx] && SolveRecursive(target - numbers[idx], numbers, idx - 1, part2)) return true;
-        if (!part2) return false;
+        if (idx == 0) return target == numbers[0];
+
+        long currentNumber = numbers[idx];
+        if (CanDivide(target, currentNumber) && SolveRecursive(target / currentNumber, numbers, idx - 1, part2)) return true;
+        if (CanSubtract(target, currentNumber) && SolveRecursive(target - currentNumber, numbers, idx - 1, part2)) return true;
+        if (!part2 || !CanStripSuffix(target, currentNumber, out long nextTarget)) return false;
+        return SolveRecursive(nextTarget, numbers, idx - 1, part2);
+    }
+
+
+    private static bool CanDivide(long target, long number) => target % number == 0;
+    private static bool CanSubtract(long target, long number) => target > number;
+
+    private static bool CanStripSuffix(long target, long number, out long nextTarget)
+    {
         string targetStr = target.ToString();
-        string lastStr = numbers[idx].ToString();
-        if (targetStr.Length <= lastStr.Length || !targetStr.EndsWith(lastStr)) return false;
-        string nextTarget = targetStr[..^lastStr.Length];
-        return SolveRecursive(long.Parse(nextTarget), numbers, idx - 1, part2);
+        string numberStr = number.ToString();
+
+        if (targetStr.Length > numberStr.Length && targetStr.EndsWith(numberStr))
+        {
+            nextTarget = long.Parse(targetStr[..^numberStr.Length]);
+            return true;
+        }
+
+        nextTarget = 0;
+        return false;
     }
 }
 

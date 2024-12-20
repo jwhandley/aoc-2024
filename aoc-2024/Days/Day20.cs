e7b1018291d8580c7e50dfa88410b6abf8dc585a
John Handley <jwhandley17@gmail.com>
2024-12-20 13:19:58 +0000

    Consolidate solving and path finding

--- aoc-2024/Days/Day20.cs
+++ aoc-2024/Days/Day20.cs
@@ -7,7 +7,7 @@ public class Day20 : BaseDay
     private readonly Vector2 start;
     private readonly Vector2 end;
     private readonly HashSet<Vector2> walls;
-    private readonly List<Vector2> path;
+    private readonly Dictionary<Vector2, int> path;
     private readonly int width;
     private readonly int height;
 
@@ -24,9 +24,9 @@ public class Day20 : BaseDay
         walls = new HashSet<Vector2>();
         height = Input.Split('\n').Length;
         width = Input.Split('\n')[0].Length;
-        foreach (var (r, line) in Input.Split(Environment.NewLine).Index())
+        foreach ((int r, string line) in Input.Split(Environment.NewLine).Index())
         {
-            foreach (var (c, ch) in line.Index())
+            foreach ((int c, char ch) in line.Index())
             {
                 if (ch == '#') walls.Add(new Vector2(c, r));
                 if (ch == 'S') start = new Vector2(c, r);
@@ -34,58 +34,50 @@ public class Day20 : BaseDay
             }
         }
 
-        path = BreadthFirstSearch();
+        path = GetPath();
     }
 
-    private List<Vector2> BreadthFirstSearch()
+    private Dictionary<Vector2, int> GetPath()
     {
-        Queue<Vector2> queue = [];
-        queue.Enqueue(start);
-        List<Vector2> found = [];
-        HashSet<Vector2> visited = [];
+        Dictionary<Vector2, int> distances = [];
+        distances[start] = 0;
+        var current = start;
 
-        while (queue.Count > 0)
+        while (current != end)
         {
-            var current = queue.Dequeue();
-            if (!visited.Add(current)) continue;
-            found.Add(current);
-
-            if (current == end) return found;
-
-            foreach (var dir in directions)
-            {
-                if (!walls.Contains(current + dir) && InBounds(current + dir))
-                {
-                    queue.Enqueue(current + dir);
-                }
-            }
+            var dir = directions.First(d =>
+                !walls.Contains(current + d)
+                && InBounds(current + d)
+                && !distances.ContainsKey(current + d)
+            );
+            
+            var next = current + dir;
+            if (distances.ContainsKey(next)) continue;
+            distances[next] = distances[current] + 1;
+            current = next;
         }
 
-        return found;
+        return distances;
     }
 
     private bool InBounds(Vector2 point) => point.X >= 0 && point.X < width && point.Y >= 0 && point.Y < height;
     private static int ManhattanDist(Vector2 a, Vector2 b) => (int)Math.Abs(a.X - b.X) + (int)Math.Abs(a.Y - b.Y);
 
-    public override long Part1()
+    private int Solve(int maxDistance)
     {
-        Dictionary<Vector2, int> visited = path.Index().ToDictionary(p => p.Item, p => p.Index);
-
-        int cost = path.Count;
         int result = 0;
 
-        for (int i = 0; i < path.Count - 1; i++)
+        foreach ((var point, int dist) in path)
         {
-            for (int dx = -2; dx <= 2; dx++)
+            for (int dx = -maxDistance; dx <= maxDistance; dx++)
             {
-                for (int dy = -2; dy <= 2; dy++)
+                for (int dy = -maxDistance; dy <= maxDistance; dy++)
                 {
-                    var other = path[i] + new Vector2(dx, dy);
-                    if (!visited.TryGetValue(other, out int j)) continue;
-                    int distance = ManhattanDist(path[i], other);
-                    if (distance != 2) continue;
-                    int newCost = i + cost - j + distance;
-                    if (cost - newCost >= 100) result++;
+                    var other = point + new Vector2(dx, dy);
+                    if (!path.TryGetValue(other, out int j)) continue;
+                    int distance = ManhattanDist(point, other);
+                    if (distance < 2 || distance > maxDistance) continue;
+                    if (j - dist >= 100 + distance) result++;
                 }
             }
         }
@@ -93,22 +85,6 @@ public class Day20 : BaseDay
         return result;
     }
 
-    public override long Part2()
-    {
-        int cost = path.Count;
-        int result = 0;
-
-        for (int i = 0; i < path.Count - 1; i++)
-        {
-            for (int j = i + 1; j < path.Count; j++)
-            {
-                int dist = ManhattanDist(path[i], path[j]);
-                if (dist > 20) continue;
-                int newCost = i + cost - j + dist;
-                if (cost - newCost >= 100) result++;
-            }
-        }
-
-        return result;
-    }
+    public override long Part1() => Solve(2);
+    public override long Part2() => Solve(20);
 }
